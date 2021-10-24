440f9a38bb5517eaf2dccdb78ac63618b8de07a8
David Ramirez Ledesma <davo.rmz@gmail.com>
2021-10-24 13:29:32 -0500

    hashTable delete and keys methods added

--- src/data-structures/hash-table.js
+++ src/data-structures/hash-table.js
@@ -19,6 +19,47 @@ class HashTable{
         this.data[address].push([key, value])
         return this.data;
     }
+
+    get(key){
+        const address = this.hashMethod(key);
+        const currentBucket = this.data[address];
+        if(currentBucket){
+            for(let i=0; i < currentBucket.length; i++){
+                if(currentBucket[i][0] === key)
+                    return currentBucket[i][1];
+            }
+        }
+        return undefined;
+    }
+
+    delete(key){
+        const address = this.hashMethod(key);
+        const currentBucket = this.data[address];
+        let item = null;
+        if(currentBucket){
+            for(let i=0; i < currentBucket.length; i++){
+                if(currentBucket[i][0] === key)
+                    item = currentBucket[i][1];
+                    delete currentBucket[i]
+                    return item;
+            }
+        }
+        return undefined;
+    }
+
+    keys(){
+        //TODO: look for a better aproach
+        const keys = [];
+        for(let i = 0; i < this.data.length; i++){
+            if(this.data[i]){
+                for(let j = 0; j < this.data[i].length; j++){
+                    if(this.data[i][j])
+                        keys.push(this.data[i][j][0]);
+                }
+            }
+        }
+        return keys;
+    }
 }
 
 export default HashTable;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import MyArray from "./data-structures/array.js";
-
+import HashTable from "./data-structures/hash-table.js";
 const myArray = new MyArray();
 
+const hashTable = new HashTable(10);
+ hashTable.set("e1", 2000);
+ hashTable.set("e2", 2002);
+ hashTable.set("e3", 2003);
 
-myArray.push("e1");
-myArray.push("e2");
-myArray.push("e3");
-myArray.push("e4");
-console.log('Array : ', myArray);
-myArray.dequeueu();
+ console.log('Hash: ', hashTable);
+ hashTable.delete('e2');
+ console.log('Hash: ', hashTable);
 
-console.log('Array: ', myArray);
+ console.log('keys', hashTable.keys());
+
+// myArray.push("e1");
+// myArray.push("e2");
+// myArray.push("e3");
+// myArray.push("e4");
+// console.log('Array : ', myArray);
+// myArray.dequeueu();
+
+// console.log('Array: ', myArray);
+
 
