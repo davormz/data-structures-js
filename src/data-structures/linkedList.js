d72fa56e935a9feafc6b6e4c4f3ac5a5f8616ae4
David Ramirez Ledesma <davo.rmz@gmail.com>
2021-11-15 22:04:57 -0600

    middle insertions added

--- src/data-structures/linkedList.js
+++ src/data-structures/linkedList.js
@@ -27,4 +27,37 @@ class SinlglyLinkedList{
         this.length++;
         return this;
     }
+
+    insert(index, value){
+        const newNode = new Node(value);
+        let previousNode = this.getByIndex(index - 1);
+        newNode.next = previousNode.next;
+        previousNode.next = newNode;
+        this.length ++;
+        return this;
+    }
+
+    getByIndex(index){
+        let node = this.head;
+        let i = 0;
+        while(i <= index && index < this.length){
+            if(i === index){
+                return node;
+            } else { 
+                node = node.next;
+                i++;
+            }
+        }
+        return undefined;       
+    }
+
+    delete(index){
+        const previousNode = this.getByIndex(index - 1);
+        let node = previousNode.next;
+        previousNode.next = node.next;
+        this.length --;
+        return this;
+    }
 }
+
+export default SinlglyLinkedList;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import MyArray from "./data-structures/array.js";
 import HashTable from "./data-structures/hash-table.js";
+import SinlglyLinkedList from "./data-structures/linkedList.js";
+
 const myArray = new MyArray();
 
 const hashTable = new HashTable(10);
- hashTable.set("e1", 2000);
- hashTable.set("e2", 2002);
- hashTable.set("e3", 2003);
+//  hashTable.set("e1", 2000);
+//  hashTable.set("e2", 2002);
+//  hashTable.set("e3", 2003);
 
- console.log('Hash: ', hashTable);
- hashTable.delete('e2');
- console.log('Hash: ', hashTable);
+//  console.log('Hash: ', hashTable);
+//  hashTable.delete('e2');
+//  console.log('Hash: ', hashTable);
 
- console.log('keys', hashTable.keys());
+//  console.log('keys', hashTable.keys());
 
 // myArray.push("e1");
 // myArray.push("e2");
@@ -22,4 +24,16 @@ const hashTable = new HashTable(10);
 
 // console.log('Array: ', myArray);
 
+
+const linkedList = new SinlglyLinkedList('n1');
+linkedList.append('n2');
+linkedList.prepend('n0');
+linkedList.append('n3');
+
+console.log(linkedList);
+console.log('Node at index 2: ', linkedList.getByIndex(2));
+console.log('insert: ', linkedList.insert(2, 'n4'));
+console.log(linkedList.getByIndex(2));
+console.log('delete: ', linkedList.delete(2));
+console.log(linkedList.getByIndex(2));
 
