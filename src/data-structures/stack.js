d678209edf5cb439ac2faf64b437aec6129f75ec
David Ramirez Ledesma <davo.rmz@gmail.com>
2021-12-03 21:53:26 -0600

    Stack class added

---
+++ src/data-structures/stack.js
@@ -0,0 +1,37 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack{
+    constructor(){
+        this.top = null;
+        this.botom = null;
+        this.length = 0;
+    }
+
+    peek(){
+        return this.top;
+    }
+
+    push(value){
+        const newNode = new Node(value);
+        if(this.length === 0){
+            this.top = newNode;
+            this.botom = newNode;
+        } else {
+            const temp = this.top;
+            this.top = newNode;
+            this.top.next = temp;
+        }
+        this.length++;
+
+        return this;
+    }
+
+    pop(){
+        //TODO
+    }
+}
